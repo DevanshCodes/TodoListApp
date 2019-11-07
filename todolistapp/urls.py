08502ffa967498ebd33df37e85267fe8f6de96ce
Devansh Agarwal <devanshagarwal50@gmail.com>
2019-11-07 14:33:53 +0530

    User input inabled

--- todolistapp/urls.py
+++ todolistapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('add',views.addtodo,name='add'),
+    path('complete/<todo_id>', views.completed, name='complete'),
+    path('delete', views.deleteAll, name='delete'),
+    path('delcomp', views.deleteComplete, name='delcomp')
 ]
--- todolistapp/views.py
+++ todolistapp/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Todo
+from .forms import TodoForm
 # Create your views here.
 
 def index(request):
+    form = TodoForm()
     todo_list = Todo.objects.order_by('id')
-    context= {'todo' : todo_list}
+    context= {'todo' : todo_list, 'form':form}
 
     return render(request,'todolistapp/index.html',context)
+
+def addtodo(request):
+    form = TodoForm(request.POST)
+
+    
+    new_todo = Todo(text=request.POST['text'])
+    new_todo.save()
+    return redirect('index')
+
+def completed(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.complete = True
+    todo.save()
+    return redirect('index')
+
+def deleteAll(request):
+    Todo.objects.all().delete()
+    return redirect('index')
+
+
+def deleteComplete(request):
+    Todo.objects.filter(complete = True).delete()
+    return redirect('index')
